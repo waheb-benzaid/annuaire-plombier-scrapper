3eb16370f85480070ad3991d52af1ffaac54ce36
waheb-benzaid <waheb.benzaid@gmail.com>
2024-03-07 18:50:06 +0100

    add website url

---
+++ __main__.py
@@ -0,0 +1,7 @@
+import selenium
+import selenium.webdriver
+import selenium.webdriver.common.by
+import selenium.webdriver.support.ui
+import selenium.webdriver.common.keys
+
+website_url = "https://annuaire-plombier-france.fr/"
---
+++ departments.py
@@ -0,0 +1,8 @@
+departments_list = ["auvergne-rhone-alpes",
+                    "bourgogne-franche-comte",
+                    "bretagne",
+                    "centre-val-de-loire",
+                    "ajaccio-bastia-corse",
+                    "grand-est",
+                    "hauts-de-france",
+                    "","","","","","","","","","","",]
