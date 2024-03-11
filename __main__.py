5efd7baf57b8ac281ed81f804e309a32a0de6b35
waheb-benzaid <waheb.benzaid@gmail.com>
2024-03-11 15:23:20 +0100

    add departemnts to the code base

--- __main__.py
+++ __main__.py
@@ -1,7 +1,20 @@
-import selenium
-import selenium.webdriver
-import selenium.webdriver.common.by
-import selenium.webdriver.support.ui
-import selenium.webdriver.common.keys
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By as by
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.keys import Keys
+import time
+import csv
+import re
+import departments
 
-website_url = "https://annuaire-plombier-france.fr/"
+
+def main():
+    driver = webdriver.Edge()
+    website_url = "https://annuaire-plombier-france.fr/"
+    driver.get(website_url)
+    # acceptCookies(driver)
+    driver.implicitly_wait(30)
+    element = driver.find_element(by.CLASS_NAME, value='')
+    print()
+
